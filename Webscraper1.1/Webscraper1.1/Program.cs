4ec5fd5a78bebd18d30be52bfb8a2beb5ef6fab8
aleksandar-dole <servicesicret@gmail.com>
2020-05-12 21:24:33 +0200

    Web scraper added

---
+++ Webscraper1.1/Webscraper1.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webscraper1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetHtmlAsync();
+            Console.ReadLine();
+        }
+        private static async void GetHtmlAsync()
+        {
+            var url = "https://www.indeed.com/jobs?l=aurora%2C%20il&radius=0&sort=date&start=20&vjk=c9396f74158c1e6d";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var JobsHtml = htmlDocument.DocumentNode.Descendants(0).Where(n => n.HasClass("jobsearch-SerpJobCard")).ToList();
+            
+            foreach (var job in JobsHtml)
+            {
+             job.Descendants("h2").Where(n => n.HasClass("title")).FirstOrDefault();
+            }
+             //JobsHtml[0].Descendants("h2").Where(n => n.HasClass("title")).ToList();
+
+            //Descendants(0).Where(n => n.HasClass("title"));
+
+            //var JobsListItems = JobsTitle[0].Descendants("a")
+            //    .Where(node => node.GetAttributeValue("id", "")
+            //    .Contains("jl").ToList();
+
+            Console.WriteLine();
+           
+            
+        }
+    }
+}
